f8ba14498a3532bdaa68f68088cdc99e06b6eb6d
shoooone <pride.a.s.w.12.01@gmail.com>
2020-01-20 23:20:22 +0900

    backend initial commit

---
+++ maternity-backend/src/main/java/com/example/maternitybackend/MaternityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.maternitybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaternityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaternityBackendApplication.class, args);
+	}
+
+}
---
+++ maternity-backend/src/test/java/com/example/maternitybackend/MaternityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maternitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaternityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
