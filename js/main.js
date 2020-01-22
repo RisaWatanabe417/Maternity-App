6585f2014592bcece7ffb24f8b82593a13c49154
RisaWatanabe417 <aquarius19840122@gmail.com>
2020-01-22 14:18:28 +0900

    ToDoリスト追加機能実装

--- js/main.js
+++ js/main.js
@@ -4,11 +4,18 @@
   var vm = new Vue({
     el: '#app',
     data: {
+      newItem: '',
       todos: [
         'task1',
         'task2',
         'task3'
       ]
-    }
+    },
+    methods: {
+      addItem: function() {
+        this.todos.push(this.newItem);
+        this.newItem = "";
+      }
+    } 
   });
 })();
